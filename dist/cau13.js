1e460e06ab834ba05895edd365869d27c2980ad8
loc2605 <locphanhuu2605@gmail.com>
2025-08-20 16:01:49 +0700

    cau13

---
+++ dist/cau13.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Shape {
+}
+class Square extends Shape {
+    side;
+    constructor(side) { super(); this.side = side; }
+    area() { return this.side * this.side; }
+}
+class Circle extends Shape {
+    radius;
+    constructor(radius) { super(); this.radius = radius; }
+    area() { return Math.PI * this.radius * this.radius; }
+}
+const square = new Square(4);
+console.log("Square area:", square.area());
+const circle = new Circle(3);
+console.log("Circle area:", circle.area());
---
+++ src/cau13.ts
@@ -0,0 +1,21 @@
+abstract class Shape {
+  abstract area(): number;
+}
+
+class Square extends Shape {
+  side: number;
+  constructor(side: number) { super(); this.side = side; }
+  area(): number { return this.side * this.side; }
+}
+
+class Circle extends Shape {
+  radius: number;
+  constructor(radius: number) { super(); this.radius = radius; }
+  area(): number { return Math.PI * this.radius * this.radius; }
+}
+
+const square = new Square(4);
+console.log("Square area:", square.area());
+
+const circle = new Circle(3);
+console.log("Circle area:", circle.area());
