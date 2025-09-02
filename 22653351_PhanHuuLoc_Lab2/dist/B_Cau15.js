095ed55e866a1527158dcf6e57b1b24806c7aae8
loc2605 <locphanhuu2605@gmail.com>
2025-09-02 21:10:19 +0700

    B Cau15

---
+++ 22653351_PhanHuuLoc_Lab2/dist/B_Cau15.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function multiplyAfter1s(num) {
+    await new Promise((res) => setTimeout(res, 1000));
+    return num * 3;
+}
+async function runSequential() {
+    console.log("15: Sequential start");
+    const a = await multiplyAfter1s(2);
+    console.log("15: Result A =", a);
+    const b = await multiplyAfter1s(3);
+    console.log("15: Result B =", b);
+    const c = await multiplyAfter1s(4);
+    console.log("15: Result C =", c);
+}
+runSequential();
